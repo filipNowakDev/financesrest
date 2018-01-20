0992c210a6592d0f30cd649049459c71f81b0709
jankanapka <filiposn@gmail.com>
2018-01-20 22:37:31 +0100

    Initial commit

---
+++ src/main/java/com/filip/financesrest/FinancesrestApplication.java
@@ -0,0 +1,12 @@
+package com.filip.financesrest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinancesrestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinancesrestApplication.class, args);
+	}
+}
---
+++ src/test/java/com/filip/financesrest/FinancesrestApplicationTests.java
@@ -0,0 +1,16 @@
+package com.filip.financesrest;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class FinancesrestApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
